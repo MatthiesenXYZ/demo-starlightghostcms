9833f5a5cc356c008273b2417e15a9b315882162
Adam Matthiesen <amatthiesen@outlook.com>
2024-02-20 07:27:35 -0800

    working

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,15 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import starlightGhostCMS from '@matthiesenxyz/starlight-ghostcms';
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [
 		starlight({
 			title: 'My Docs',
+			plugins: [starlightGhostCMS()],
 			social: {
-				github: 'https://github.com/withastro/starlight',
+				github: 'https://github.com/matthiesenxyz/astro-ghostcms',
 			},
 			sidebar: [
 				{
